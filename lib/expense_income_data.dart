6758032e7e7b959f02a47ed023983ce010f54bee
Mohammed Hussain <mohammed.dvlpr@gmail.com>
2024-09-15 01:11:54 +0530

    Design 3 Adding date week and drawer

--- lib/expense_income_data.dart
+++ lib/expense_income_data.dart
@@ -7,23 +7,99 @@ class ExpenseIncomeData extends StatefulWidget {
   State<ExpenseIncomeData> createState() => _ExpenseIncomeDataState();
 }
 
+// income and expense design
 class _ExpenseIncomeDataState extends State<ExpenseIncomeData> {
+  final List itemList = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  String selectedValue = "August";
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-      child: Column(
-        children: [
-          expenceInconeContainer(context, Icons.arrow_upward, "Expenes",
-              4100.00, Colors.pink, Colors.white),
-          const SizedBox(
-            height: 10,
+    return Expanded(
+      child: SizedBox(
+        height: 240,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  expenceInconeContainer(context, Icons.arrow_upward, "Expenes",
+                      4100.00, Colors.pink, Colors.white),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  expenceInconeContainer(context, Icons.arrow_downward,
+                      "Income", 2100.00, Colors.white, Colors.pink),
+                ],
+              ),
+              SizedBox(
+                width: 150,
+                child: Column(children: [
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        dwm(
+                          "Day",
+                        ),
+                        dwm("Week"),
+                        dwm("Month", colors: Colors.pink)
+                      ]),
+                  const SizedBox(height: 10),
+                  Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      height: 28,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade900,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton(
+                          value: selectedValue,
+                          dropdownColor: Colors.black,
+                          items:
+                              itemList.map<DropdownMenuItem<String>>((value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(
+                                value,
+                                style: TextStyle(color: Colors.grey.shade500),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              selectedValue = newValue!;
+                            });
+                          },
+                        ),
+                      ))
+                ]),
+              )
+            ],
           ),
-          expenceInconeContainer(context, Icons.arrow_downward, "Income",
-              2100.00, Colors.white, Colors.pink)
-        ],
+        ),
       ),
     );
+  }
+
+// days week and month design
+  Widget dwm(String text, {Color? colors}) {
+    return Text(text, style: TextStyle(color: colors, fontSize: 15));
   }
 
   Widget expenceInconeContainer(BuildContext context, IconData icon,
